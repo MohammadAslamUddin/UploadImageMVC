540058449b2b88e10ceb4aa8b77db42181136f14
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-09-12 17:54:22 +0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UploadImageMVC.Startup))]
+namespace UploadImageMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

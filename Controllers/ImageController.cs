70df3afc26d027f60ece7cf5684f2599a7465810
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-09-12 18:13:09 +0600

    Azura Database Added

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace UploadImageMVC.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
